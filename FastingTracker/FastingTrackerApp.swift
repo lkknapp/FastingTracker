ff56242842fb0da3360623b5a4cf95cd245ab779
Linda Knapp <linda@bozuk.org>
2024-03-06 10:20:39 -0800

    Initial Commit

---
+++ FastingTracker/FastingTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  FastingTrackerApp.swift
+//  FastingTracker
+//
+//  Created by Linda Knapp on 3/6/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FastingTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FastingTrackerUITests/FastingTrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FastingTrackerUITestsLaunchTests.swift
+//  FastingTrackerUITests
+//
+//  Created by Linda Knapp on 3/6/24.
+//
+
+import XCTest
+
+final class FastingTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
