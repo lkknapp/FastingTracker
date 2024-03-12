2f29a7f3b9402a81daaabbf0271746d5fc299b6a
Linda Knapp <linda@bozuk.org>
2024-03-12 09:30:54 -0700

    V_1.0-Initial_Design

---
+++ FastingTracker/FastingManager.swift
@@ -0,0 +1,8 @@
+//
+//  FastingManager.swift
+//  FastingTracker
+//
+//  Created by Linda Knapp on 3/6/24.
+//
+
+import Foundation
---
+++ FastingTracker/ProgressRing.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressRing.swift
+//  FastingTracker
+//
+//  Created by Linda Knapp on 3/6/24.
+//
+
+import SwiftUI
+
+struct ProgressRing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressRing()
+}
